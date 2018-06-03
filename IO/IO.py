dfe9650df63add346e34907a9eb61cf2625252e3
Justin Le <justinhle@hotmail.com>
2018-06-02 23:02:13 -0700

    test code

--- IO/IO.py
+++ IO/IO.py
@@ -14,11 +14,11 @@ ledPinGreen = 13    # RPI Board pin5
 switchPin = 15 # 
 
 def setupIO():
-	GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
-	GPIO.setup(ledPinRed, GPIO.OUT)   # Set ledPinRed's mode is output
-	GPIO.output(ledPinRed, GPIO.LOW) # Set ledPinRed low to off led
-        GPIO.setup(ledPinGreen, GPIO.OUT)   # Set ledPinGreen's mode is output
-	GPIO.output(ledPinGreen, GPIO.LOW) # Set ledPinGreen low to off led
+##	GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
+##	GPIO.setup(ledPinRed, GPIO.OUT)   # Set ledPinRed's mode is output
+##	GPIO.output(ledPinRed, GPIO.LOW) # Set ledPinRed low to off led
+##        GPIO.setup(ledPinGreen, GPIO.OUT)   # Set ledPinGreen's mode is output
+##	GPIO.output(ledPinGreen, GPIO.LOW) # Set ledPinGreen low to off led
         GPIO.setup(switchPin, GPIO.IN)   # Set ledPinGreen's mode is output
 
 	#print 'using pin%d'%ledPin
@@ -36,10 +36,12 @@ def redLedOff:
         GPIO.output(ledPinRed, GPIO.LOW)  # led on
 
 def checkSwitchMode(): #Low = True = in home High= False = security
-        if GPIO.input(switchPin)==GPIO.LOW:
-                return True
-        else :
-                return False
+        print GPIO.input(switchPin)
+
+##        if GPIO.input(switchPin)==GPIO.LOW:
+##                return True
+##        else :
+##                return False
 
 def destroyAll():
 	GPIO.output(ledPinRed, GPIO.LOW)     # led off
